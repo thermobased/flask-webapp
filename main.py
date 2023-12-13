cf52984d52c05ad16596769205e561425e6bde1f
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-12-13 10:33:47 +0100

    simple form sending

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route("/login", methods = ["POST"])
+def login():
+    u = request.form["username"]
+    p = request.form["password"]
+    print(f"{u=} {p=}")
+    return f"""
+    You entered {u=}, {p=}
+    """
 
 @app.route("/")
 def hello_world():
     # hey
-    return "<p>Hello, World!</p>"
+    return """
+    <form action="/login" method="post">
+        Username:
+        <input type="text" id="username" name="username" required>
+
+        <br>
+        Password:
+        <input type="password" id="password" name="password" required>
+
+        <br>
+        <input type="submit" value="Login">
+    </form>
+    """
