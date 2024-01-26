06bd31c2ac6c2ce3d7e3dc053f24955f7212683c
Andrey Oleinikov <oleinikov.2000.ao@gmail.com>
2024-01-26 15:20:09 +0600

    added habits section

--- create.sql
+++ create.sql
@@ -1,4 +1,3 @@
 CREATE TABLE  items (id INTEGER PRIMARY KEY, login TEXT UNIQUE, password BLOB);
-INSERT INTO items(login, password) VALUES('admin', 'admin');
-CREATE TABLE sessions (login TEXT UNIQUE, cookie_key TEXT);
-
+CREATE TABLE habits (login TEXT, habit TEXT, checkmark INTEGER);
+CREATE TABLE sessions (login TEXT, cookie_key TEXT);
--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from jinja2 import Template
 import sqlite3, random, string, hashlib
 from sqlite3 import IntegrityError
 
-app = Flask(__name__)
+assert sqlite3.threadsafety == 3
 
+app = Flask(__name__)
 database = None
 
 
 def get_db():
     global database
     if database is None:
-        database = sqlite3.connect("collection.db")
+        database = sqlite3.connect("collection.db", check_same_thread=False)
     return database
 
 
@@ -34,8 +35,7 @@ def register():
         return redirect(url_for('main', error=f"{e}"))
 
 
-
-def makekey():
+def make_key():
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(20))
     return result_str
@@ -56,53 +56,85 @@ def login():
     if i.fetchone() is None:
         resp = make_response(redirect(url_for('main')))
     else:
-        cookie_key = makekey()
+        cookie_key = make_key()
         cur.execute("INSERT INTO sessions(login, cookie_key) VALUES(?, ?)", (u, cookie_key))
         resp = make_response(redirect(url_for('profile')))
         resp.set_cookie('authkey', cookie_key)
     return resp
 
 
-@app.route('/profile')
+@app.route("/profile", methods=['POST', 'GET'])
 def profile():
-    authkey =  request.cookies.get('authkey')
-    con = get_db()
-    cur = con.cursor()
-    i = cur.execute("""
-                SELECT login FROM sessions WHERE cookie_key = ?
-                """, (authkey,))
-    welcome = i.fetchone()
-    if welcome is not None:
-        body = render_template("welcome.html", name=welcome[0])
+    global user
+    if request.method == 'GET':
+        authkey = request.cookies.get('authkey')
+        con = get_db()
+        cur = con.cursor()
+        i = cur.execute("""
+                    SELECT login FROM sessions WHERE cookie_key = ?
+                    """, (authkey,))
+        welcome = i.fetchone()
+        user = welcome[0]
+        i = cur.execute("""
+                    SELECT habit FROM habits WHERE login = ?
+                    """, welcome)
+        habits = i.fetchall()
 
-        return render_template("main.html", title="Profile", body=body)
-    else:
-        return redirect(url_for('main'))
+        #       reducing list of tuples to list of strings
+        for q in range(0, len(habits)):
+            habits[q] = habits[q][0]
+
+        if welcome is not None:
+            body = render_template("welcome.html", habits=habits)
+
+            return render_template("main.html", title="Profile", body=body, name=welcome[0])
+        else:
+            return redirect(url_for('main'))
+
+    if request.method == 'POST':
+        new_habit = request.form["new_habit"]
+        con = get_db()
+        cur = con.cursor()
+        cur.execute("INSERT INTO habits (login, habit, checkmark) VALUES(?, ?, ?)", (user, new_habit, 0))
+        con.commit()
+        resp = redirect(url_for('profile'))
+        return resp
 
 
-@app.route("/logout", methods=["POST"])
+@app.route("/logout", methods=['POST'])
 def logout():
     con = get_db()
     cur = con.cursor()
     cooka = request.cookies.get('authkey')
     if cooka is not None:
-        i = cur.execute("SELECT cookie_key FROM sessions WHERE (cookie_key) =?", (cooka, ))
+        i = cur.execute("SELECT cookie_key FROM sessions WHERE (cookie_key) =?", (cooka,))
         if i.fetchone() is not None:
-            cur.execute("DELETE FROM sessions WHERE cookie_key = ?", (cooka, ))
+            cur.execute("DELETE FROM sessions WHERE cookie_key = ?", (cooka,))
 
     resp = make_response(redirect(url_for("main")))
     resp.set_cookie('authkey', 'null')
     return resp
 
 
+@app.route("/delete_habit", methods=['POST'])
+def delete_habit():
+    habit_delete = request.form["habit_name"]
+    con = get_db()
+    cur = con.cursor()
+    i = cur.execute("SELECT habit FROM habits where (habit) =?", (habit_delete, ))
+    if i.fetchone() is not None:
+        cur.execute("DELETE FROM habits WHERE habit = ?", (habit_delete,))
+    resp = make_response(redirect(url_for("profile")))
+    return resp
+
+
 @app.route("/", methods=["GET"])
 def main():
-    loggedin = request.cookies.get('authkey')
+    logged_in = request.cookies.get('authkey')
     error = f"{request.args.get('error')}"
-    # hey
     body = f"""
-    <div style="color:red">Error: {error}</div>
-    Login:
+    
+    
     <form action="/login" method="post">
         Username:
         <input type="text" id="username" name="username" required>
@@ -114,7 +146,7 @@ def main():
         <br>
         <input type="submit" value="Login">
     </form>
-    Register:
+    
     <form action="/register" method="post">
         Username:
         <input type="text" id="username" name="username" required>
@@ -126,6 +158,7 @@ def main():
         <br>
         <input type="submit" value="Register">
     </form>
+    <div class = "error">Error: {error}</div>
     
         
     """
@@ -133,8 +166,7 @@ def main():
     cur = con.cursor()
     i = cur.execute("""
             SELECT cookie_key FROM sessions WHERE cookie_key = ?
-            """, (loggedin, ))
+            """, (logged_in,))
     if i.fetchone() is not None:
         return redirect(url_for('profile'))
     return render_template("main.html", title="Main page", body=body)
-
