971b62303ddda3d38fc08145b4e49b193b0b1029
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-02-21 09:30:42 +0100

    indent CREATE TABLE statements to make the easier to skip through

--- create.sql
+++ create.sql
@@ -1,10 +1,32 @@
-CREATE TABLE  items (id INTEGER PRIMARY KEY, login TEXT UNIQUE, password BLOB);
-CREATE TABLE sessions (login TEXT, cookie_key TEXT, FOREIGN KEY(login) REFERENCES items(login));
-CREATE TABLE habits (id INTEGER PRIMARY KEY, login TEXT, habit TEXT, UNIQUE(login, habit) FOREIGN KEY(login) REFERENCES items(login));
+CREATE TABLE items (
+  id INTEGER PRIMARY KEY,
+  login TEXT UNIQUE,
+  password BLOB
+);
 
-CREATE TABLE datapoints (login TEXT, habit TEXT, chislo TEXT, datapoint INTEGER, comment TEXT,
-FOREIGN KEY(login, habit) REFERENCES habits(login, habit)
-UNIQUE(login, habit, chislo));
+CREATE TABLE sessions (
+  login TEXT,
+  cookie_key TEXT,
+  FOREIGN KEY(login) REFERENCES items(login)
+);
+
+CREATE TABLE habits (
+  id INTEGER PRIMARY KEY,
+  login TEXT,
+  habit TEXT,
+  UNIQUE(login, habit)
+  FOREIGN KEY(login) REFERENCES items(login)
+);
+
+CREATE TABLE datapoints (
+  login TEXT,
+  habit TEXT,
+  chislo TEXT,
+  datapoint INTEGER,
+  comment TEXT,
+  FOREIGN KEY(login, habit) REFERENCES habits(login, habit)
+  UNIQUE(login, habit, chislo)
+);
 
 --CREATE TABLE days (habit INTEGER, den TEXT, FOREIGN KEY (habit) REFERENCES habits(id));
 
