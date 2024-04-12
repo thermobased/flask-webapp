b2a2b9640550318196e868bdd9b29254499cb6fe
Andrey Oleinikov <oleinikov.2000.ao@gmail.com>
2024-04-12 14:04:52 +0500

    implementing timeline chart

---
+++ frontend/render_graph.ts
@@ -0,0 +1,26 @@
+import { XYContainer, Timeline } from '@unovis/ts'
+import moment from "moment"
+declare var collection: any;
+
+type TimeDataRecord = {
+    timestamp: number; // Position on the X axis. Can be `number` or `Date`
+    length: number; // Length of the line in X axis values, i.e. milliseconds if you use `Date`
+    type: string; // The row it will be displayed in
+}
+
+let data: TimeDataRecord[] = [];
+for(let i = 0; i<collection.length; i++){
+    data.push(
+        {timestamp: moment(collection[i][1], "YY, M, D").unix()/24/60/60, length: collection[i][2], type: i.toString()},
+             )
+}
+console.log(data);
+const node = document.getElementById('graph');
+const container = new XYContainer<TimeDataRecord>(node, {
+    components: [
+        new Timeline<TimeDataRecord>({
+            x: (d: TimeDataRecord) => d.timestamp,
+            showLabels: true
+        })
+    ]
+}, data)
