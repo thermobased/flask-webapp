e3da3e004357a84d955c003cb6083abf528f95b8
Andrey Oleinikov <oleinikov.2000.ao@gmail.com>
2024-06-11 22:55:23 +0500

    added third week to calendar and grey cells for future days

--- frontend/render_calendar.ts
+++ frontend/render_calendar.ts
@@ -8,8 +8,12 @@ export function renderCalendar() {
     }
     
     function createCalendarCell(i: number){
+        let pastToday: boolean = false;
         const weekday = document.createElement('button');
         weekday.setAttribute('value', moment().day(i).format("YY, M, D"));
+        if(i > moment().day()){
+            weekday.setAttribute('id', 'past_todays');
+        }
         weekday.setAttribute('class', 'dates');
             let kek = weekday.value;
             weekday.addEventListener('click', (ev) => {
@@ -24,6 +28,7 @@ export function renderCalendar() {
             console.log(dayOfWeek, " <-- day(i)");
             if(moment().date() == dayOfWeek){
                 weekday.setAttribute('id', 'todays');
+                pastToday = true;
             }
             weekday.innerHTML = dayOfWeek.toString();
             return weekday;
@@ -31,25 +36,32 @@ export function renderCalendar() {
 
     const firstWeek = document.querySelector(".first_week_row")!;
     const secondWeek = document.querySelector(".second_week_row")!;
+    const thirdWeek = document.querySelector(".third_week_row")!;
         firstWeek.innerHTML = '';
         secondWeek.innerHTML = '';
+        thirdWeek.innerHTML = '';
 
 //-------- if today is sunday ----------
     if(moment().day() == 0){
+        for(let i = -20; i<=-14; i++){
+            firstWeek.appendChild(createCalendarCell(i));
+        }
+        for(let i = -13; i<=-7; i++){
+            secondWeek.appendChild(createCalendarCell(i));
+        }
+        for(let i = -6; i<=0; i++){
+            thirdWeek.appendChild(createCalendarCell(i));
+        }
+//-------- if today is not sunday ----------
+    } else {
         for(let i = -13; i<=-7; i++){
             firstWeek.appendChild(createCalendarCell(i));
         }
         for(let i = -6; i<=0; i++){
             secondWeek.appendChild(createCalendarCell(i));
         }
-//-------- if today is not sunday ----------
-    } else {
-        for(let i = -6; i<=0; i++){
-            firstWeek.appendChild(createCalendarCell(i));
-        }
-        
         for(let i = 1; i<=7; i++){
-            secondWeek.appendChild(createCalendarCell(i));
+            thirdWeek.appendChild(createCalendarCell(i));
         }
     }
 
